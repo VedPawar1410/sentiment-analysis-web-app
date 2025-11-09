ec476e4de3e71dc0b82d2f79236a2e769128dc0e
Ved Pawar <vedpawar1410@gmail.com>
2025-11-09 15:12:31 +0530

    commented main fn, will deploy on live server, gunicorn

--- app.py
+++ app.py
@@ -30,5 +30,5 @@ def predict():
     return render_template('result.html', prediction=result)
 
 # Run the app
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+    #app.run(debug=True)
